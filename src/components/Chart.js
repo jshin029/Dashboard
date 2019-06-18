758396ffd149d7bdd19454839a909c7ec8a9b2b7
John Shin <jshin029@ucr.edu>
2019-06-18 11:26:12 -0700

    react setup

---
+++ src/components/Chart.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import [Bar,Line,Pie] from 'react-chart-js-2';
+
+class Chart extends Component {
+  render(){
+    return (
+      <div className="chart">
+        Chart
+      </div>
+    )
+  }
+}
