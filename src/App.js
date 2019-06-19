633a2e9b0c2a4c1f8c57f213e8fe074e38b7fad3
John Shin <jshin029@ucr.edu>
2019-06-19 13:30:13 -0700

    default chart component added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Chart from "./components/Chart"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    };
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    //Ajax calls here
+    this.setState({
+      chartData:{
+
+          labels: ['h7', 'h2', 'h3', 'h4', 'h5'],
+          datasets: [
+            {
+              label:'Population',
+              data:[
+                60000,
+                50000,
+                40000,
+                30000,
+                20000
+              ],
+              backgroundColor:[
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(255, 99, 132, 0.6)'
+              ]
+            }
+          ]
+
+      }
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+      <Chart chartData={this.state.chartData} location="California"/>
+      </div>
+    );
+  }
+};
 
 export default App;
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,12 +1,40 @@
 import React, {Component} from 'react';
-import [Bar,Line,Pie] from 'react-chart-js-2';
+import {Bar,Line,Pie} from 'react-chartjs-2';
 
 class Chart extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      chartData:props.chartData
+    }
+  }
+
+  static defaultProps = {
+    displayTitle:true,
+    displayLegend:true,
+    legendPosition:'right',
+    location:'Location'
+  }
+
   render(){
     return (
       <div className="chart">
-        Chart
+        <Bar
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Location is in: ' + this.props.location,
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:'right'
+            }
+          }}
+        />
       </div>
-    )
+    );
   }
-}
+};
+
+export default Chart;
